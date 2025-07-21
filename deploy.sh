72fa02d025cc4204ec37ccc885a19e4d8b081bac
ShreyasVH <shreyas.deve.1@gmail.com>
2025-07-21 11:36:06 +0530

    safe start

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,5 @@
-./dist/bin/interceptor -jvm-debug "7070" -Dhttp.port=9070 > interceptor.log 2>&1 &
-# ./target/universal/stage/bin/interceptor -jvm-debug "7070" -Dhttp.port=9070 > interceptor.log 2>&1 &
+if ! test -f "dist/RUNNING_PID"
+then
+	./dist/bin/interceptor -jvm-debug "7070" -Dhttp.port=9070 > interceptor.log 2>&1 &
+	# ./target/universal/stage/bin/interceptor -jvm-debug "7070" -Dhttp.port=9070 > interceptor.log 2>&1 &
+fi
