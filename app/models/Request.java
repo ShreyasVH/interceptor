d3e2e6c77183a88679712d33e1166e9588b8caa0
ShreyasVH <shreyas.deve.1@gmail.com>
2026-01-13 00:38:15 +0530

    using play 3.0.10

--- app/models/Request.java
+++ app/models/Request.java
@@ -1,22 +1,22 @@
 package models;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.databind.JsonNode;
-import io.ebean.Model;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.*;
-import java.util.Date;
+import jakarta.persistence.*;
 
 @Entity
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Table(name = "requests")
 @Getter
 @Setter
-public class Request extends Model
+@NoArgsConstructor
+public class Request
 {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String host;
@@ -27,9 +27,9 @@ public class Request extends Model
 
     private String method;
 
-    private JsonNode payload;
+    private String payload;
 
-    private JsonNode headers;
+    private String headers;
 
     @OneToOne(cascade = CascadeType.ALL)
     private Response response;
--- app/models/Response.java
+++ app/models/Response.java
@@ -2,26 +2,24 @@ package models;
 
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.databind.JsonNode;
-import io.ebean.Model;
+import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Entity
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Table(name = "responses")
 @Getter
 @Setter
-public class Response extends Model
+@NoArgsConstructor
+public class Response
 {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private JsonNode headers;
+    private String headers;
 
     private int status;
 
